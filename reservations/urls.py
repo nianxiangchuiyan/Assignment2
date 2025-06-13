946bbefb28efda0abccad1c7a8a9a9ef34e114ad
nianxiangchuiyan <161560763+nianxiangchuiyan@users.noreply.github.com>
2025-06-13 12:53:05 +1200

    test

--- reservations/urls.py
+++ reservations/urls.py
@@ -23,11 +23,12 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .api_view import RoomViewSet, ReservationViewSet, UserViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+
+from .api_view import RoomViewSet, ReservationViewSet, UserViewSet
 
 router = DefaultRouter()
 router.register(r'rooms', RoomViewSet)
