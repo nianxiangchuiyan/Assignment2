7d98bfe3fa7c52b98bdf3632a0a9374a34624218
nianxiangchuiyan <161560763+nianxiangchuiyan@users.noreply.github.com>
2025-06-16 12:03:10 +1200

    CORS

--- Assignment1/settings.py
+++ Assignment1/settings.py
@@ -43,11 +43,11 @@ INSTALLED_APPS = [
     'reservations',
     'rest_framework',
     'rest_framework_simplejwt',
-
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
-
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -147,5 +147,6 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
     "*",
 ]
